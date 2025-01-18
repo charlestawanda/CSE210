b5cc69dbb5efa9cad8c55529839788324cbf830f
charlestawanda <tsamatanda@gmail.com>
2025-01-18 11:09:31 +0200

    we23

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,13 +1,11 @@
 using System;
 using System.ComponentModel;
 
-// Added a mood query, added logic to handle a type error in choosing a function in the main menu.
-// Added logic to ensure each prompt is only used once per session and handled the possible index out of range exception.
 class Program
 {
     static void Main(string[] args)
     {
-        string rizz = "<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+        string rizz = "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
         Console.WriteLine($"\n{rizz}\nHi, I'm Jemeima, your journal buddie. Let's get started.\n");
 
         Journal j1 = new Journal();
@@ -31,7 +29,7 @@ class Program
                 prompts.AddPrompts();
             }
 
-            Console.WriteLine("\nPlease choose one of the following:");
+            Console.WriteLine("\nPlease select one of the following number to get started:");
             Console.Write("1. Add New Entry\n2. Display Journal\n3. Save\n4. Load\n5. Stop\n> ");
             string usrResponse = Console.ReadLine();
             if (usrResponse == "1" || usrResponse == "2" || usrResponse == "3" || usrResponse == "4" || usrResponse == "5")
@@ -45,7 +43,7 @@ class Program
                     entry.Display();
                     j1.AddEntry(entry);
 
-                    // Removes the selected prompt from the list of prompts
+                
                     prompts._prompts.Remove(entry._promptText);
                 }
                 else if (response == 2)
@@ -69,12 +67,12 @@ class Program
                 else if (response == 5)
                 {
                     write = false;
-                    Console.WriteLine("\nThank you fortaking the time to tell me about your day, cheers.");
+                    Console.WriteLine("\nThank you fortaking the time to tell me about your day,we hope that your day will blessed, cheers.");
                     Console.WriteLine(rizz);
                 }
                 else
                 {
-                    Console.WriteLine("Invalid choice");
+                    Console.WriteLine("error! invalid choise");
                 }
 
             }
